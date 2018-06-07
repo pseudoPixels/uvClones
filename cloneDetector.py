3892263d65ab136dc9ff9e644901fd2c23c4386c
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-06-07 10:34:19 -0600

    Bug fixes regarding nan

--- cloneDetector.py
+++ cloneDetector.py
@@ -9,9 +9,8 @@ import pybrain
 import uuid
 import subprocess
 import os
-
-
-
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 #load the trained Neural Net
@@ -104,13 +103,9 @@ def similaritiesNormalizedByLine(sourceCode1, sourceCode2, lang):
 	txlFilePath = 'txl_features/java/normalizeLiteralsToZero.txl'
 	type3sim_by_line = getCodeCloneSimilarity(sourceCode1, sourceCode2, lang, txlFilePath)
 
-	#out = {'type_1_similarity_by_line':type1sim_by_line, 'type_2_similarity_by_line':type2sim_by_line,'type_3_similarity_by_line':type3sim_by_line}
+	
 
 	return type1sim_by_line, type2sim_by_line, type3sim_by_line
-
-	#print 'type_1_similarity_by_line', type1sim_by_line
-
-	#return jsonify({'error_msg': 'None', 'log_msg': 'Preprocessing Source Codes...\nNormalizing Source Codes...\nCalculating Similarities...\nDone.','output': out})
 
 
 
@@ -126,10 +121,10 @@ def similaritiesNormalizedByToken(sourceCode1, sourceCode2, lang):
 	txlFilePath = 'txl_features/java/normalizeLiteralsToZero.txl'
 	type3sim_by_token = getCodeCloneSimilarity(sourceCode1, sourceCode2, lang, txlFilePath)
 
-	#out = {'type_1_similarity_by_token':type1sim_by_token, 'type_2_similarity_by_token':type2sim_by_token,'type_3_similarity_by_token':type3sim_by_token}
+	
 
 	return type1sim_by_token, type2sim_by_token, type3sim_by_token
-	#return jsonify({'error_msg': 'None', 'log_msg': 'Preprocessing Source Codes...\nNormalizing Source Codes...\nCalculating Similarities...\nDone.','output': out})
+	
 
 
 
@@ -144,31 +139,18 @@ def similaritiesNormalizedByToken(sourceCode1, sourceCode2, lang):
 def getValidationScore(sourceCode1, sourceCode2, lang):
 
 	type1sim_by_line, type2sim_by_line, type3sim_by_line = similaritiesNormalizedByLine(sourceCode1, sourceCode2, lang)
+	#print type1sim_by_line, type2sim_by_line, type3sim_by_line
+
 	type1sim_by_token, type2sim_by_token, type3sim_by_token = similaritiesNormalizedByToken(sourceCode1, sourceCode2, lang)
+	#print type1sim_by_token, type2sim_by_token, type3sim_by_token
 
-	network_prediction = loaded_fnn.activate([type1sim_by_line, type2sim_by_line, type3sim_by_line, type1sim_by_token, type2sim_by_token, type3sim_by_token])
+	network_prediction = loaded_fnn.activate([type2sim_by_line, type2sim_by_line, type3sim_by_line, type1sim_by_token, type2sim_by_token, type3sim_by_token])
 
-	print 'true_clone_probability_score ==> ', network_prediction[1]
-	print 'false_clone_probability_score ==> ', network_prediction[0]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	#print 'true_clone_probability_score ==> ', network_prediction[1]
+	#print 'false_clone_probability_score ==> ', network_prediction[0]
+	
+	#return true probability score
+	return 	network_prediction[1]
 
 
 
@@ -182,99 +164,138 @@ def getValidationScore(sourceCode1, sourceCode2, lang):
 #Clone Detector Calling
 
 sourceCode1 = '''
-/**
- * A class to provide a simple list of integers.
- * List resizes automatically. Used to illustrate 
- * various design and implementation details of 
- * a class in Java.
- * 
- * Version 1 only contains the instance variables and
- * the constructors
- * @author scottm
- *
- */
-public class IntListVer1 {
-    // class constant for default size
-    private static final int DEFAULT_CAP = 10;
-    
-    //instance variables
-    private int[] iValues;
-    private int iSize;
-    
-    /**
-     * Default constructor. Creates an empty list.
-     */
-    public IntListVer1(){
-        //redirect to single int constructor
-        this(DEFAULT_CAP);
-        //other statments could go here.
-    }
-	public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
-		for(int i = 0; i < NUM_FACTS; i++)
-			System.out.println( i + "! is " + factorial(i));
-	}
-	
-	public static int factorial(int n)
-	{	int result = 1;
-		for(int i = 2; i <= n; i++)
-			result *= i;
-		return result;
-	}
-    /**
-     * Constructor to allow user of class to specify 
-     * initial capacity in case they intend to add a lot
-     * of elements to new list. Creates an empty list.
-     * @param initialCap > 0
-     */    
-    public IntListVer1(int initialCap) {
-        assert initialCap > 0 : "Violation of precondition. IntListVer1(int initialCap):"
-            + "initialCap must be greater than 0. Value of initialCap: " + initialCap;
-        iValues = new int[initialCap];
-        iSize = 0;
-    }
+protected DrawingView createDrawingView () {
+    DrawingView createdDrawingView = createDrawingView (createDrawing ());
+    createdDrawingView.drawing ().setTitle (getDefaultDrawingTitle ());
+    return createdDrawingView;
 }
 '''
 
 sourceCode2 = '''
-public class Factorial
-{
-	public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
-		for(int i = 0; i < NUM_FACTS; i++)
-			System.out.println( i + "! is " + factorial(i));
-	}
-	
-	public static int factorial(int n)
-	{	int result = 1;
-		for(int i = 2; i <= n; i++)
-			result *= i;
-		return result;
-	}
+public Tool getDefaultTool () {
+    if (fDefaultToolButton != null) {
+        return fDefaultToolButton.tool ();
+    } else {
+        return null;
+    }
 }
 '''
 
 lang = 'java'
 
 #getValidationScore(sourceCode1, sourceCode2, lang)
-
-
-
-
-
 #execTxl('txl_features/java/PrettyPrint.txl', sourceCode1, lang)
-
-
-
 #getCodeCloneSimilarity(sourceCode1, sourceCode2, lang, 'txl_features/java/PrettyPrint.txl')
-
-
-
 #similaritiesNormalizedByToken(sourceCode1, sourceCode2, lang)
+#getValidationScore(sourceCode1, sourceCode2, lang)
 
 
-getValidationScore(sourceCode1, sourceCode2, lang)
 
+
+
+
+###############
+### parsing and iterating over the potential clones
+###############
+
+# tree1 = ET.parse('jHotDraw2.xml')
+# root1 = tree1.getroot()
+#
+# tree2 = ET.parse('jHotDraw2.xml')
+# root2 = tree2.getroot()
+
+
+
+# for child1 in root1:
+# 	src1 = child1.text
+# 	for child2 in root2:
+# 		src2 = child2.text
+# 		if child1.attrib != child2.attrib:
+# 			trueCloneProb = getValidationScore(src1, src2, 'java')
+# 			#print trueCloneProb
+# 			if trueCloneProb > 0.5:
+# 				print '***************************** CLONE PAIR ***************************'
+# 				print src1
+# 				print ' =========================== '
+# 				print src2
+
+#
+# minLine = 6
+# maxLine = 10000
+#
+#
+
+# for i in range(0, len(root1)):
+# 	src1 = root1[i].text
+# 	for j in range(i+1, len(root1)):
+# 		src2 = root1[j].text
+# 		trueCloneProb = getValidationScore(src1, src2, 'java')
+# 		#print trueCloneProb
+# 		if trueCloneProb > 0.5:
+# 			print '***************************** CLONE PAIR ***************************', trueCloneProb
+# 			print src1
+# 			print ' =========================== '
+# 			print src2
+
+
+potential_clones = 'jHotDraw2.xml'
+minLine = 11
+maxLine = 2500
+
+
+
+
+
+
+soup = ''
+with open(potential_clones) as fp:
+	soup = BeautifulSoup(fp, 'lxml')
+
+all_potential_clones = soup.find_all('source')
+
+l = len(all_potential_clones)
+print l
+c = 0
+for i in range(0, len(all_potential_clones)):
+	src1 = all_potential_clones[i].text
+	src1_lines = src1.count('\n')
+	#print "Progress ", i*100/l , "%"
+	for j in range(i+1, len(all_potential_clones)):
+		src2 = all_potential_clones[j].text
+		src2_lines = src2.count('\n')
+
+		if (src1_lines >= minLine or src2_lines >= minLine) and (src1_lines <= maxLine or src2_lines <= maxLine):
+			if min(src1_lines, src2_lines)*2 >= max(src1_lines, src2_lines):
+				#print "HERE ", src1
+				#print " src1 => ", src1_lines, " src2 => ", src2_lines
+				# #print "src1 => ", src1.count('\n'), " src2 => ", src2.count('\n'), " min=> ", min(src1.count('\n'), src2.count('\n'))
+				trueCloneProb = getValidationScore(src1, src2, 'java')
+				#print '++++++++++++++++++++++++++++++++++++++'
+				#print src1
+				#print src2
+				#print trueCloneProb
+				#print '++++++++++++++++++++++++++++++++++++++'
+				#print trueCloneProb
+				if trueCloneProb > 0.5:
+					c = c + 1
+					print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', trueCloneProb
+					print 'system_id'
+					print 'clone_id'
+					print all_potential_clones[i].attrs['file'], " ", all_potential_clones[i].attrs['startline'], " ", all_potential_clones[i].attrs['endline']
+					print all_potential_clones[j].attrs['file'], " ", all_potential_clones[j].attrs['startline'], " ", all_potential_clones[j].attrs['endline']
+
+					print '----------------------------------------'
+					print src1
+					print '----------------------------------------'
+					print src2
+					print '----------------------------------------'
+
+					# print '***************************** CLONE PAIR ***************************', trueCloneProb
+					# print src1
+					# print ' =========================== '
+					# print src2
+
+print "Total Clones Found:: ", c
 
 #################################################################################
 ####################CODE CLONE VALIDATION ENDS###################################
